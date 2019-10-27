153607f49fe0a39d5605ae9cd5aa0e9ce9601bc4
duyanhpham <duyanh.y4n.pham@gmail.com>
2019-10-27 22:15:26 +0100

    add first main program for project with both communication and visualisierung

---
+++ Quadrocopter_pi_controller_main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File            : Quadrocopter_pi_controller_main.py
+# Author          : Duy Anh Pham <duyanh.y4n.pham@gmail.com>
+# Date            : 27.10.2019
+# Last Modified By: Duy Anh Pham <duyanh.y4n.pham@gmail.com>
+
+import serial
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+#####################################################
+# port config
+#####################################################
+uart_bytesize = 8
+uart_parity = 'N'
+uart_baudrate = 9600
+uart_stopbit = 1
+
+ser = serial.Serial('/dev/ttyUSB0')
+ser.bytesize = uart_bytesize
+ser.parity = uart_parity
+ser.baudrate = uart_baudrate
+ser.stopbits = uart_stopbit
+
+print(
+    "open " + ser.name + "\nbaud: " + str(ser.baudrate) + "\ndata format:" + str(ser.bytesize) + str(ser.parity) + str(
+        ser.stopbits))
+
+#####################################################
+# Init Visualization
+#####################################################
+max_sample_len = 20
+x = np.linspace(-max_sample_len+1, 0, max_sample_len)
+y_max = 255
+y_min = 0
+x_max = 0
+x_min = -max_sample_len
+y = np.zeros(len(x))
+text_last_value = "last value: " + str(y[-1])
+tex_align_x = 0.15
+tex_align_y = 0.95
+
+# You probably won't need this if you're embedding things in a tkinter plot...
+plt.ion()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_ylim([y_min, y_max])
+ax.set_xlim([x_min, x_max])
+ax.set_ylabel("Sensor data")
+ax.set_xlabel("Sample")
+ax.set_title("Test realtime visualization")
+plot_text = ax.text(tex_align_x, tex_align_y, text_last_value, horizontalalignment='center',
+        verticalalignment='center',
+        transform=ax.transAxes)
+line1, = ax.plot(x, y, 'r-')  # Returns a tuple of line objects, thus the comma
+
+#####################################################
+# Main programm loop
+#####################################################
+s = [0]
+while True:
+    # update data
+    read_serial = ser.read()
+    y[:-1] = y[1:]
+    y[-1] = int.from_bytes(read_serial, byteorder="big")
+    text_last_value = "last value: " + str(y[-1])
+    # update plot
+    plot_text.set_text(text_last_value)
+    line1.set_ydata(y)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    # print(read_serial.hex()) # test show received byte

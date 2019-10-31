2c69900a6079973141358f4df00cd5d016357de3
duyanhpham <duyanh.y4n.pham@gmail.com>
2019-10-31 20:56:56 +0100

    add visual quadro complete with filter

--- Quadrocopter_pi_controller_main.py
+++ Quadrocopter_pi_controller_main.py
@@ -18,7 +18,7 @@ import math
 #####################################################
 uart_bytesize = 8
 uart_parity = 'N'
-uart_baudrate = 9600
+uart_baudrate = 115200
 uart_stopbits = 1
 ser = Serial_begin(uart_bytesize, uart_parity,
                    uart_baudrate, uart_stopbits)
@@ -37,8 +37,8 @@ x_min = -max_sample_len
 xlim = [x_min, x_max]
 y_JoystickRX = np.zeros(len(x))
 y_JoystickRY = np.zeros(len(x))
-y_max = 255
-y_min = 0
+y_max = 16500
+y_min = -16500
 ylim = [y_min, y_max]
 text_last_value = 'last value '
 text_align_x = 0.25
@@ -80,57 +80,160 @@ axis_plot_text_RY = Plot_add_text(axis_plot_RY,
 plot_line_RY, = Plot_add_line(axis_plot_RY, x, y_JoystickRY, 'r-')
 
 ########### subplot Neigung ############
-x_neigung = np.array([-1,0,1])
+x_neigung = np.array([-1, 0, 1])
 winkel_neigung = 0
 y_neigung = np.zeros(len(x_neigung))
 xlim_neigung = [-1, 1]
-ylim_neigung = [-2, 2]
+ylim_neigung = [-1, 1]
 neigung_plot = data_visual_figure.add_subplot(223)
 Plot_set_axis(neigung_plot,
               xlim_neigung, ylim_neigung,
               xlabel='', ylabel='',
-              title='Neigung')
-neigung_plot.tick_params(
-    axis='x',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom=False,      # ticks along the bottom edge are off
-    top=False,         # ticks along the top edge are off
-    labelbottom=False) # labels along the bottom edge are o   labelbottom=False) # labels along the bottom edge are off
-neigung_plot.tick_params(
-    axis='y',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom=False,      # ticks along the bottom edge are off
-    top=False,         # ticks along the top edge are off
-    labelbottom=False) # labels along the bottom edge are o   labelbottom=False) # labels along the bottom edge are off
-
+              title='')
+neigung_plot.axes.get_xaxis().set_visible(False)
+neigung_plot.axes.get_yaxis().set_visible(False)
+neigung_plot_text = Plot_add_text(neigung_plot, 0.5, 0.95, 'Neigung', 'center', 'center')
 plot_line_neigung, = Plot_add_line(neigung_plot, x_neigung, y_neigung, 'b-')
 
+
+########### subplot Rollen ############
+x_rollen = np.array([-1, 0, 1])
+winkel_rollen = 0
+y_rollen = np.zeros(len(x_rollen))
+xlim_rollen = [-1, 1]
+ylim_rollen = [-1, 1]
+rollen_plot = data_visual_figure.add_subplot(224)
+Plot_set_axis(rollen_plot,
+              xlim_rollen, ylim_rollen,
+              xlabel='', ylabel='',
+              title='')
+rollen_plot.axes.get_xaxis().set_visible(False)
+rollen_plot.axes.get_yaxis().set_visible(False)
+rollen_plot_text = Plot_add_text(rollen_plot, 0.5, 0.95, 'Rollen', 'center', 'center')
+plot_line_rollen, = Plot_add_line(rollen_plot, x_rollen, y_rollen, 'b-')
+
+
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
+
+
+MAX_SPRUNG = 16384
+
+
+def filter_convert_data():
+   global MAX_SPRUNG
+   global x_raw
+   global y_raw
+   global z_raw
+   global x_raw_alt
+   global y_raw_alt
+   global z_raw_alt
+   global ax_g
+   global ay_g
+   global az_g
+   if abs(x_raw-x_raw_alt) < MAX_SPRUNG:
+        ax_g = float(x_raw/16384)
+   else:
+        ax_g = float(x_raw_alt/16384)
+
+   if abs(y_raw-y_raw_alt) < MAX_SPRUNG:
+        ay_g = float(y_raw/16384)
+   else:
+        ay_g = float(y_raw_alt/16384)
+
+   if abs(z_raw-z_raw_alt) < MAX_SPRUNG:
+        az_g = float(z_raw/16384)
+   else:
+        az_g = float(z_raw_alt/16384)
+
+   ax_g = constrain(ax_g, -1, 1)
+   ay_g = constrain(ay_g, -1, 1)
+   az_g = constrain(az_g, -1, 1)
+
+
+first_loop = True
+
+
+def get_accel_data():
+   global first_loop
+   global x_raw
+   global y_raw
+   global z_raw
+   global x_raw_alt
+   global y_raw_alt
+   global z_raw_alt
+   global ax_g
+   global ay_g
+   global az_g
+   if first_loop == True:
+        x_raw_alt = x_raw
+        y_raw_alt = y_raw
+        z_raw_alt = z_raw
+        first_loop = False
+   filter_convert_data()
+   x_raw_alt = x_raw
+   y_raw_alt = y_raw
+   z_raw_alt = z_raw
+
+
+def calculate_angle():
+   global nicken_grad
+   global rollen_grad
+   global ax_g
+   global ay_g
+   global az_g
+   nicken_grad = -(math.atan2(ay_g, az_g)*57.3)
+   rollen_grad = math.atan2(
+        (-ax_g), math.sqrt(ay_g * ay_g + az_g * az_g)) * 57.3
+
+
+x_raw_alt = 0
+y_raw_alt = 0
+z_raw_alt = 0
+x_raw = 0
+y_raw = 0
+z_raw = 0
+ax_g = 0.0
+ay_g = 0.0
+az_g = 0.0
+nicken_grad = 0.0
+rollen_grad = 0.0
 #####################################################
 # Main programm loop
 #####################################################
 while True:
     # wait for
-    ser.reset_input_buffer()
-    while np.not_equal(np.frombuffer(ser.read(), dtype=np.uint8), DATA_HEADER_VALUE[0]):
-        pass
-    #check if header is correct
-    if np.equal(np.frombuffer(ser.read(), dtype=np.uint8), DATA_HEADER_VALUE[1]):
         # read data as bytes array from serial device (arduino)
-        new_data = np.frombuffer(ser.read(DATA_BODY_START), dtype=np.uint8)
+    ser.reset_input_buffer()
+    read_data = ser.readline()
+    if len(read_data) > 0:
+        new_data = str(read_data).split(',')
 
-        # update data arrays index:")
-        y_JoystickRX[:-1] = y_JoystickRX[1:]
-        y_JoystickRX[-1] = int.from_bytes(new_data[0], byteorder=sys.byteorder)
-        y_JoystickRY[:-1] = y_JoystickRY[1:]
-        y_JoystickRY[-1] = int.from_bytes(new_data[1], byteorder=sys.byteorder)
-        winkel_neigung = y_JoystickRX
-        y_neigung = np.tan(np.deg2rad(winkel_neigung))[0]*x_neigung
-    
-        # update plot
-        axis_plot_text_RX.set_text(text_last_value + str(y_JoystickRX[-1]))
-        axis_plot_text_RY.set_text(text_last_value + str(y_JoystickRY[-1]))
-        plot_line_RX.set_ydata(y_JoystickRX)
-        plot_line_RY.set_ydata(y_JoystickRY)
-        plot_line_neigung.set_ydata(y_neigung)
+    if len(new_data) > 1:
+        if new_data[1] == 'data':
+            x_raw = int(new_data[2])
+            y_raw = int(new_data[3])
+            z_raw = int(new_data[4])
+            get_accel_data()
+            calculate_angle()
+            # update data arrays index:")
+            y_JoystickRX[:-1] = y_JoystickRX[1:]
+            y_JoystickRX[-1] = x_raw
+            y_JoystickRY[:-1] = y_JoystickRY[1:]
+            y_JoystickRY[-1] = y_raw
+            winkel_neigung = nicken_grad
+            winkel_rollen = rollen_grad
+            y_neigung = np.tan(np.deg2rad(winkel_neigung))*x_neigung
+            y_rollen = np.tan(np.deg2rad(winkel_rollen))*x_rollen
 
-        Plot_figure_update(data_visual_figure)
+            # # update plot
+            axis_plot_text_RX.set_text(text_last_value + str(y_JoystickRX[-1]))
+            axis_plot_text_RY.set_text(text_last_value + str(y_JoystickRY[-1]))
+            neigung_plot_text.set_text('Neigung ' + str(int(winkel_neigung)))
+            rollen_plot_text.set_text('Rollen ' + str(int(winkel_rollen)))
+            plot_line_RX.set_ydata(y_JoystickRX)
+            plot_line_RY.set_ydata(y_JoystickRY)
+            plot_line_neigung.set_ydata(y_neigung)
+            plot_line_rollen.set_ydata(y_rollen)
+
+            Plot_figure_update(data_visual_figure)

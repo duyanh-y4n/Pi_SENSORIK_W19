2d01e0ed4bb704f0abae32b67f0fafffad0dfb6d
duyanhpham <duyanh.y4n.pham@gmail.com>
2019-11-13 12:09:32 +0100

    refactor + add update text

--- bokeh_app_main.py
+++ bokeh_app_main.py
@@ -10,7 +10,7 @@ from random import random
 from threading import Thread
 import time
 
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Title
 from bokeh.plotting import curdoc, figure
 from bokeh.layouts import gridplot
 from tornado import gen
@@ -60,6 +60,11 @@ winkel_rollen_filtert = 0.0
 time_current = time.time()*1000
 time_start_app = time.time()*1000
 
+#####################################################
+# main process - get data
+#####################################################
+
+
 def get_data():
     global y_JoystickRX, y_JoystickRY, y_neigung, y_rollen, y_neigung_filtert, y_rollen_filtert
     global time_current, winkel_neigung, winkel_rollen, winkel_neigung_filtert, winkel_rollen_filtert
@@ -69,7 +74,7 @@ def get_data():
     winkel_min = -45
     winkel_max = 45
     # wait for
-        # read data as bytes array from serial device (arduino)
+    # read data as bytes array from serial device (arduino)
     ser.reset_input_buffer()
     read_data = ser.readline().decode().strip()
     # print(read_data)
@@ -77,21 +82,23 @@ def get_data():
 
     if (len(new_data) > 0) and (new_data[0] == 'data'):
         sample_time = time.time()*1000 - time_current
-        print("\nSample time: " + str(int(sample_time))+ ' ms')
+        print("\nSample time: " + str(int(sample_time)) + ' ms')
         print("data [header,x_raw,y_raw,z_raw]")
         print(new_data)
         x_raw = int(new_data[1])
         y_raw = int(new_data[2])
         z_raw = int(new_data[3])
         get_accel_data(x_raw, y_raw, z_raw)[0]
-        winkel_neigung,winkel_rollen, winkel_neigung_filtert,winkel_rollen_filtert = calculate_angle()
-        pwm_rx = (winkel_rollen_filtert-winkel_min)*(pwm_max-pwm_min)/(winkel_max-winkel_min)+pwm_min
-        pwm_ry = (winkel_neigung_filtert-winkel_min)*(pwm_max-pwm_min)/(winkel_max-winkel_min)+pwm_min
-        global send_sting 
+        winkel_neigung, winkel_rollen, winkel_neigung_filtert, winkel_rollen_filtert = calculate_angle()
+        pwm_rx = (winkel_rollen_filtert-winkel_min) * \
+            (pwm_max-pwm_min)/(winkel_max-winkel_min)+pwm_min
+        pwm_ry = (winkel_neigung_filtert-winkel_min) * \
+            (pwm_max-pwm_min)/(winkel_max-winkel_min)+pwm_min
+        global send_sting
         send_string = str(pwm_rx) + "X" + str(pwm_ry) + "YE"
         ser.write(send_string.encode("utf-8"))
-        
-        print("winkel [nicken,rollen]") 
+
+        print("winkel [nicken,rollen]")
         print([winkel_neigung, winkel_rollen])
         # update data arrays index:
         y_JoystickRX[1:] = y_JoystickRX[:-1]
@@ -100,10 +107,16 @@ def get_data():
         y_JoystickRY[0] = pwm_ry
         y_neigung = np.tan(np.deg2rad(winkel_neigung))*x_neigung
         y_rollen = np.tan(np.deg2rad(winkel_rollen))*x_rollen
-        y_neigung_filtert = np.tan(np.deg2rad(winkel_neigung_filtert))*x_neigung
+        y_neigung_filtert = np.tan(np.deg2rad(
+            winkel_neigung_filtert))*x_neigung
         y_rollen_filtert = np.tan(np.deg2rad(winkel_rollen_filtert))*x_rollen
         time_current = time.time()*1000
         return pwm_rx, pwm_ry
+
+
+#####################################################
+# second  process - visualization
+#####################################################
 
 ################ define datasource for visualization ####################
 # this must only be modified from a Bokeh session callback
@@ -114,35 +127,52 @@ source2 = ColumnDataSource(data=dict(x=[0], y=[0]))
 # see the same document.
 doc = curdoc()
 
+##################### configure plots ##################
+p1 = figure(x_axis_label="time in ms")
+l1 = p1.line(x='x', y='y', source=source1)
+title1 = Title(text="Nicken", align="center")
+p1.add_layout(title1, "above")
+value1 = Title(text="", align="center")
+p1.add_layout(value1, "left")
+
+p2 = figure(x_axis_label="time in ms")
+l2 = p2.line(x='x', y='y', source=source2)
+title2 = Title(text="Rollen", align="center")
+p2.add_layout(title2, "above")
+value2 = Title(text="", align="center")
+p2.add_layout(value1, "left")
+
+# grid = gridplot([[p1, p2]], plot_height=250, plot_width=250)
+grid = gridplot([[p1, p2]], sizing_mode='stretch_both')
+doc.add_root(grid)
+
+
+#####################################################
+# Main program
+#####################################################
 ###################### define tasks ##################
 # updae visualisation
 @gen.coroutine
 def update(x, y1, y2):
-    # time.sleep(0.01)
-    source1.stream(dict(x=[x], y=[y1]), rollover=30)
-    source2.stream(dict(x=[x], y=[y2]), rollover=30)
+    global value1,value2
+    value1.text = str(int(y1))
+    value2.text = str(int(y2))
+    source1.stream(dict(x=[x], y=[y1]), rollover=50)
+    source2.stream(dict(x=[x], y=[y2]), rollover=50)
 
 # get data + filter data
+
+
 def blocking_task():
     while True:
         # do some blocking computation
-        # time.sleep(0.1)
         y1, y2 = get_data()
         x = int((time.time()*1000-time_start_app))
-        print("gettime" + str(x))
+        print("run time" + str(x))
 
         # but update the document from callback
         doc.add_next_tick_callback(partial(update, x=x, y1=y1, y2=y2))
 
-
-##################### configure plots ##################
-p1 = figure(title="Nicken", x_axis_label="time in ms")
-l1 = p1.line(x='x', y='y', source=source1)
-p2 = figure(title="Rollen", x_axis_label="time in ms")
-l2 = p2.line(x='x', y='y', source=source2)
-
-grid = gridplot([[p1,p2]],plot_height=250,plot_width=250)
-doc.add_root(grid)
 
 ################# start programm with defined task #################
 thread = Thread(target=blocking_task)
